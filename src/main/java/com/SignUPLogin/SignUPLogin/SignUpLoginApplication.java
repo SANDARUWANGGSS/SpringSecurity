d50d483742d28b7e03483b4e6129e142ec200e23
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-02-20 21:42:01 +0530

    Spring boot application file added

---
+++ src/main/java/com/SignUPLogin/SignUPLogin/SignUpLoginApplication.java
@@ -0,0 +1,12 @@
+package com.SignUPLogin.SignUPLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpLoginApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SignUpLoginApplication.class, args);
+    }
+}
